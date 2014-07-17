c980925a6e57e97d460f594f16809eb9671a8bed
Willy Lee <wlee@carleton.edu>
2014-07-17 07:06:23 -0700

    Squashed 'blocks/filtered_course_list/' changes from 8a8d4f5..0e51a70
    0e51a70 bumps version to 2.4.0
    e6111ba updates README.md
    c36e03f Merge branches 'CLAMP-684', 'CLAMP-686' and 'CLAMP-687' into FCL_20_dev
    05bee75 CLAMP-687 santizes custom labels for filtered course lists
    f615d37 CLAMP-686 applies regex to validate the maxallcourse setting
    aa7d083 CLAMP-684 allows a course to match multiple shortname searches
    b156c84 Merge branch 'CLAMP-669' into testing_20140418
    d788722 CLAMP-699 grays out links to hidden courses
    1ce2a7f Merge branches 'wip-CLAMP-620_master' and 'wip-CLAMP-631_master' into testing_20140418
    798e328 CLAMP-631 changes courselist to course_list in certain variable names
    7c5d3aa CLAMP-620 builds upgrade path to accommodate new settings keys
    f3f38d7 CLAMP-630 allows admins to see block under all circumstances
    088e9ec CLAMP-624 sets constants for settings values whether admin sees all or own courses
    3b57536 CLAMP-627 defines constant for default labelscount
    9248c6a CLAMP-619 explicitly tests whether term variables are empty
    31cf6fb CLAMP-629 adds option to hide block from guests and anonymous
    438c99c CLAMP-567 removes redundant $this->title assignment
    fd3650e CLAMP-628 updates URL's for category index pages
    d29b7bf CLAMP-622 adds missing standard capability myaddinstance
    609b284 CLAMP-627 adds custom shortname matching sections
    aee3222 CLAMP-625 applies 'other courses' setting for category filter
    a2443d9 CLAMP-620 clarifies some 'term'-inology
    dc2496d CLAMP-621_master bolds headings for course groupings
    9fe3224 CLAMP-624 shows admin own or all according to setting
    16022ba Fixes some white space for code checker
    5d1972f CLAMP-623 populates dropdown for admin current category
    71720db CLAMP-405 redux gets all relevant subcategories
    8f569e8 CLAMP-597 addresses codechecker style issues
    a8c755c CLAMP-619: tests that a search string exists before searching
    5126fd5 Merge branch 'CLAMP-405_master'
    cc13018 Merge branch 'CLAMP-567_master'
    175ec15 Merge branch 'CLAMP-599_master'
    b7564e4 Merge branch 'CLAMP-618_master'
    966d6aa CLAMP-405: uses subcategory logic for category filter
    7bc20c7 CLAMP-567: renames block to filtered course list with configurable block title
    ff085e7 CLAMP-599: shows category for single-category sites with many courses
    7897552 CLAMP-618: repositions course icon
    1ee0b6c CLAMP-612 sets hard defaults for fresh installs
    
    git-subtree-dir: blocks/filtered_course_list
    git-subtree-split: 0e51a703ed33220a60057aa13bec98fbd894afe6

--- settings.php
+++ settings.php
@@ -16,41 +16,92 @@
 
 defined('MOODLE_INTERNAL') || die;
 
+require_once(dirname(__FILE__) . '/locallib.php');
+
 if ($ADMIN->fulltree) {
+
+    $settings->add(new admin_setting_heading('block_filtered_course_list/general',
+        get_string('generalsettings', 'block_filtered_course_list'), ''));
+
     $settings->add(new admin_setting_configcheckbox('block_filtered_course_list_hideallcourseslink',
         get_string('hideallcourseslink', 'block_filtered_course_list'),
         get_string('confighideallcourseslink', 'block_filtered_course_list'), 0));
+
+    $settings->add(new admin_setting_configcheckbox('block_filtered_course_list_hidefromguests',
+        get_string('hidefromguests', 'block_filtered_course_list'),
+        get_string('confighidefromguests', 'block_filtered_course_list'), 0));
+
+    $settings->add(new admin_setting_configcheckbox('block_filtered_course_list_hideothercourses',
+        get_string('hideothercourses', 'block_filtered_course_list'),
+        get_string('confighideothercourses', 'block_filtered_course_list'), 0));
+
+    $settings->add(new admin_setting_configtext('block_filtered_course_list_maxallcourse',
+        get_string('maxallcourse', 'block_filtered_course_list'),
+        get_string('configmaxallcourse', 'block_filtered_course_list'), 10, '/^\d{1,3}$/', 3));
+
+    $adminviews = array(
+        BLOCK_FILTERED_COURSE_LIST_ADMIN_VIEW_ALL => get_string('allcourses', 'block_filtered_course_list'),
+        BLOCK_FILTERED_COURSE_LIST_ADMIN_VIEW_OWN => get_string('owncourses', 'block_filtered_course_list')
+    );
+
+    $settings->add(new admin_setting_configselect('block_filtered_course_list_adminview',
+        get_string('adminview', 'block_filtered_course_list'),
+        get_string('configadminview', 'block_filtered_course_list'),
+        BLOCK_FILTERED_COURSE_LIST_ADMIN_VIEW_ALL, $adminviews));
+
     $filters = array(
-        'term' => get_string('filterterm', 'block_filtered_course_list'),
+        'shortname' => get_string('filtershortname', 'block_filtered_course_list'),
         'categories' => get_string('filtercategories', 'block_filtered_course_list')
-        /* we haven't setup custom filters yet
-        2 => get_string('filtercustom','block_filtered_course_list')
-        */
     );
 
     $settings->add(new admin_setting_configselect('block_filtered_course_list_filtertype',
         get_string('filtertype', 'block_filtered_course_list'),
-        get_string('configfiltertype', 'block_filtered_course_list'), 0, $filters));
+        get_string('configfiltertype', 'block_filtered_course_list'), 'shortname', $filters));
 
-    $settings->add(new admin_setting_configtext('block_filtered_course_list_termcurrent',
-        get_string('termcurrent', 'block_filtered_course_list'),
-        get_string('configtermcurrent', 'block_filtered_course_list'), ''));
+    $settings->add(new admin_setting_heading('block_filtered_course_list/shortname',
+        get_string('shortnamesettings', 'block_filtered_course_list'),
+        get_string('shortnamesettingsinfo', 'block_filtered_course_list')));
 
-    $settings->add(new admin_setting_configtext('block_filtered_course_list_termfuture',
-        get_string('termfuture', 'block_filtered_course_list'),
-        get_string('configtermfuture', 'block_filtered_course_list'), ''));
+    $settings->add(new admin_setting_configtext('block_filtered_course_list_currentshortname',
+        get_string('currentshortname', 'block_filtered_course_list'),
+        get_string('configcurrentshortname', 'block_filtered_course_list'), ''));
 
-    $cat_list = array();
-    $categories = coursecat::get(0)->get_children();
-    if ($categories) {
-        foreach ($categories as $cat) {
-            $cat_list[$cat->id] = $cat->name;
-        }
+    $settings->add(new admin_setting_configtext('block_filtered_course_list_futureshortname',
+        get_string('futureshortname', 'block_filtered_course_list'),
+        get_string('configfutureshortname', 'block_filtered_course_list'), ''));
+
+    $howmanylabels = array();
+    for ($i = 0; $i <= 10; $i++) {
+        $howmanylabels[] = $i;
     }
+
+    $settings->add(new admin_setting_configselect('block_filtered_course_list_labelscount',
+        get_string('labelscount', 'block_filtered_course_list'),
+        get_string('configlabelscount', 'block_filtered_course_list'),
+        BLOCK_FILTERED_COURSE_LIST_DEFAULT_LABELSCOUNT, $howmanylabels));
+
+    $labelscount = BLOCK_FILTERED_COURSE_LIST_DEFAULT_LABELSCOUNT;
+    if (isset($CFG->block_filtered_course_list_labelscount)) {
+        $labelscount = $CFG->block_filtered_course_list_labelscount;
+    }
+
+    for ($i = 1; $i <= $labelscount; $i++) {
+
+        $settings->add(new admin_setting_configtext("block_filtered_course_list_customlabel$i",
+            get_string('customlabel', 'block_filtered_course_list') . " $i",
+            get_string('configcustomlabel', 'block_filtered_course_list'), '', PARAM_TEXT));
+
+        $settings->add(new admin_setting_configtext("block_filtered_course_list_customshortname$i",
+            get_string('customshortname', 'block_filtered_course_list') . " $i",
+            get_string('configcustomshortname', 'block_filtered_course_list'), ''));
+    }
+
+    $settings->add(new admin_setting_heading('block_filtered_course_list/categories',
+        get_string('categorysettings', 'block_filtered_course_list'),
+        get_string('categorysettingsinfo', 'block_filtered_course_list')));
+
+    $categories = coursecat::make_categories_list();
     $settings->add(new admin_setting_configselect('block_filtered_course_list_categories',
         get_string('categories', 'block_filtered_course_list'),
-        get_string('configcategories', 'block_filtered_course_list'), '', $cat_list));
-    $settings->add(new admin_setting_configcheckbox('block_filtered_course_list_hideothercourses',
-        get_string('hideothercourses', 'block_filtered_course_list'),
-        get_string('confighideothercourses', 'block_filtered_course_list'), 0));
+        get_string('configcategories', 'block_filtered_course_list'), 1, $categories));
 }
